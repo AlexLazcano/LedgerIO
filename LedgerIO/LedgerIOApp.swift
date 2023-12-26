e0e216d5a01034681136a200e8e51cc1626c70d1
Alex Lazcano <alazcano2000@gmail.com>
2023-12-25 21:03:29 -0800

    Initial Commit

---
+++ LedgerIO/LedgerIOApp.swift
@@ -0,0 +1,17 @@
+//
+//  LedgerIOApp.swift
+//  LedgerIO
+//
+//  Created by Alex Lazcano on 2023-12-25.
+//
+
+import SwiftUI
+
+@main
+struct LedgerIOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
